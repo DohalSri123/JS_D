8ae79881b81c5b3f5684148d8b5718a181b4012f
Dohal <dohalsri123@gmail.com>
2023-07-20 17:12:55 +0530

    Second-Commit

--- RegForm/reg.js
+++ RegForm/reg.js
@@ -1,15 +1,67 @@
+document.querySelector('#registration_form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let name;
+    let mail;
+    let password;
 
-    let textbox = document.querySelector('.form-control');
-textbox.addEventListener('keyup', function () {
-    let typemsg = textbox.value;
-    let mag = document.querySelector('#msg');
-    mag.innerHTML = typemsg;
-});
+    name = document.querySelector(".form-control").value;
+    localStorage.setItem("name", name);
+    mail = document.querySelector(".form-control_2").value;
+    localStorage.setItem("email", mail);
+    password = document.querySelector(".form-control_3").value;
+    localStorage.setItem("password", password);
+    const copy = document.querySelector(".copy");
+    copy.innerHTML = "Name:" + name + "<br>Email: " + mail + "<br>Password: " + password;
 
-let texbox = document.querySelector('.form-control_1');
-textbox.addEventListener('keyup', function () {
-    let typemsg = texbox.value;
-    let mag = document.querySelector('#mg');
-    mag.innerHTML = typemsg;
+    var storedDataSets = localStorage.getItem('dataSets');
+    var dataSets = [];
+    if (storedDataSets) {
+
+        dataSets = JSON.parse(storedDataSets);
+    }
+    var newDataSet = {
+        name: name,
+        mail: mail,
+        password: password
+    };
+    dataSets.push(newDataSet);
+    localStorage.setItem('dataSets', JSON.stringify(dataSets));
+    displayDataSets(dataSets);
 });
+function displayDataSets(dataSets) {
+    var displayData = document.querySelector(".copy");
+    displayData.innerHTML = " ";
+
+    for (var i = 0; i < dataSets.length; i++) {
+        var dataSet = dataSets[i];
+        var name = dataSet.name;
+        var mail = dataSet.mail;
+        
+
+        var dataSetElement = document.createElement("div");
+        dataSetElement.innerHTML = "Name: " + name + "<br>mail: " + mail + "<hr>";
+
+        displayData.appendChild(dataSetElement);
+    }
+}
+var storedDataSets = localStorage.getItem('dataSets');
+
+if (storedDataSets) {
+    var parsedDataSets = JSON.parse(storedDataSets);
+    displayDataSets(parsedDataSets);
+} else {
+    displayDataSets([]);
+}
+
+function checkname(){
+    var checkName = document.querySelector(".form-control").value;
+    if(checkName=== dataset.name){
+        alert("error");
+    }
+}
+
+
+
+
+
 
---
+++ callback.js
@@ -0,0 +1,14 @@
+let calculate = (a,b,operation) =>{
+    let result='';
+    if(operation=== 'sum'){
+        result = a+b;
+    }
+    if(operation==='mul'){
+        result=a*b;
+
+    }
+    return result;
+
+};
+let output = calculate(10,20,'sum');
+console.log(output);
